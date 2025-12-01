314ddaaca59846a356a0d0ed87639cd6d1284702
txnaro <thiagoferreirapires@hotmail.com>
2025-12-01 17:59:27 -0300

    Update blink_gpiozero.py

--- blink_gpiozero.py
+++ blink_gpiozero.py
@@ -1,18 +1,21 @@
 from gpiozero import LED
 from time import sleep
 
-led = LED(18) #GPIO 18
+#inicializa o objeto LED no pino GPIO 18.
+#a biblioteca gpiozero abstrai a configuração de direção (IN/OUT).
+led = LED(18) 
 
 try:
-	
-	while True:
+    #o loop infinito é essencial para que o serviço SystemD permaneça "active".
+    while True:
+        led.on()           #define nível lógico alto (3.3V)
+        print("LED on")    #log
+        sleep(1)           #temporização de 1 segundo
+        
+        led.off()          #define nível lógico baixo (0V)
+        print("LED off")
+        sleep(1)
 
-		led.on()
-		print("LED on")
-		sleep(1)
-		led.off()
-		print("LED off")
-		sleep(1)
-
+#tratamento de exceção para encerrar o script sem erros ao usar Ctrl+C manualmente
 except KeyboardInterrupt:
-	pass
+    pass
